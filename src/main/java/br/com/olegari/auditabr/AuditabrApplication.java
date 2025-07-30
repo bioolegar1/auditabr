458a90f405f50e05dc143acd9b43787ba7ce5620
bioolegar1 <bioolegari@gmail.com>
2025-07-30 14:23:20 -0300

    initializing a new project

---
+++ src/main/java/br/com/olegari/auditabr/AuditabrApplication.java
@@ -0,0 +1,13 @@
+package br.com.olegari.auditabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditabrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditabrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/olegari/auditabr/AuditabrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.olegari.auditabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
